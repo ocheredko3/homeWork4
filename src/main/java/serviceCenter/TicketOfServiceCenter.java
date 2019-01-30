f983c6ae5f9d3a4d5839bdcd145e7abc78326523
Mykhail <ocheredko3@gmail.com>
2019-01-30 08:28:22 +0200

    v 1.0

--- src/main/java/serviceCenter/TicketOfServiceCenter.java
+++ src/main/java/serviceCenter/TicketOfServiceCenter.java
@@ -1,20 +1,18 @@
 package serviceCenter;
 
-import sun.security.krb5.internal.Ticket;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class TicketOfServiceCenter {
-    public ArrayList ticketOfServiceCenter(String term) {
-        String ticketID;
-        String adminOfServiceCenter;
-        String engineerOfServiceCenter;
-        String customerOfServiceCenter;
-        String dateOfIncomeDevice;
-        String dateOfOutDevice;
-        String deviceOfServiceCenter;
-        double priceOfRepair;
+    public List<String> ticketOfServiceCenter(String term) {
+        String ticketID="703100";
+        String adminOfServiceCenter="Ivanov";
+        String engineerOfServiceCenter="sidorov";
+        String customerOfServiceCenter= "petrov";
+        String dateOfIncomeDevice="2019/12/12";
+        String dateOfOutDevice="2019/12/13";
+        String deviceOfServiceCenter="Laptpo,Loenovo,B50-80,Sn:ad121223,10000";
+        double priceOfRepair = 1000;
         List<String> TicketOfService = new ArrayList<>();
         TicketOfService.add(ticketID);
         TicketOfService.add(adminOfServiceCenter);
@@ -24,8 +22,10 @@ public class TicketOfServiceCenter {
         TicketOfService.add(dateOfOutDevice);
         TicketOfService.add(deviceOfServiceCenter);
         TicketOfService.add(String.valueOf(priceOfRepair));
-        List<TicketOfService> =new ArrayList<>();
 
 
+        return TicketOfService;
     }
+
+
 }
